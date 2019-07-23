01c211164355b9c429567af8ff516241dd69f4a8
Murthidn <murthi267@gmail.com>
2019-07-23 17:52:26 +0530

    Update

---
+++ AzureDatabricks/helper.py
@@ -0,0 +1,77 @@
+try:
+    import sys
+    import os
+
+    sys.path.insert(0, '/usr/python-packages/dn-requests/')
+    import requests
+
+    import pathlib
+    from shutil import copyfile
+
+except Exception as error:
+    print(error)
+    sys.exit(201)
+
+HOME = '/etc/sensu/plugins'
+DOMAIN = None
+TOKEN = None
+
+# REST API Access
+def clientRequest(getURL, getHeaders):
+        TIMEOUT = 60  # sec
+        try:
+            response = requests.get(getURL, headers=getHeaders, timeout=TIMEOUT)
+
+        except requests.exceptions.Timeout:
+            print('request timed out (', TIMEOUT, 'sec) <br>')
+            sys.exit(2)
+
+        except requests.exceptions.RequestException as e:
+            print('Error fetching data:', str(e))
+            sys.exit(2)
+
+        return response
+
+
+def getAuth(location, subscription):
+
+    netrc_filename='.netrc_'+location
+
+    if subscription == 'RS-Violet-Prod' or subscription == 'RS-Violet-Non-Prod':
+        netrc_filename=netrc_filename+"_india"
+    else:
+        netrc_filename=netrc_filename+"_us"
+
+    if subscription.find("Non-Prod") == -1:
+        netrc_filename = netrc_filename +"_prod"
+    else:
+        netrc_filename = netrc_filename +"_non-prod"
+
+    file = pathlib.Path(HOME+"/helpers/"+netrc_filename)
+    if file.exists():
+        for root, dirs, files in os.walk(HOME+'/helpers'):
+            if netrc_filename in files:
+                src = os.path.join(root, netrc_filename)
+                copyfile(src, HOME+'/.netrc')
+    else:
+        print("File not exist")
+        sys.exit(201)
+
+
+
+def getToken():
+    file = open(HOME + '/.netrc')
+
+    for line in file:
+        if line.strip():
+            fields = line.strip().split()
+            if fields[1] == 'token':
+                continue
+
+            elif fields[1].endswith('.azuredatabricks.net'):
+                continue
+
+            else:
+                return fields[1]
+        else:
+            continue
--- Others/First.py
+++ Others/First.py
@@ -1,15 +1,55 @@
 import sys
-sys.path.insert(0, '/usr/python-packages/dn-requests/')
-import requests
+import os
+import pathlib
+from shutil import copyfile
 
-getMetaData = requests.get("http://169.254.169.254/metadata/instance?api-version=2017-08-01", headers={'Metadata':'true'})
+DOMAIN = None
+TOKEN = None
+HOME = '/etc/sensu/plugins'
 
-if getMetaData.status_code == 200 and 'compute' in getMetaData.json() and 'location' in getMetaData.json()['compute']:
-    print("success")
-    location=getMetaData.json()['compute']['location']
-    DOMAIN = location+'.azuredatabricks.net'
-    print(DOMAIN)
+def getAuth(location, subscription):
 
+    netrc_filename='.netrc_'+location
 
-else:
-    print("Fail")
+    if subscription == 'RS-Violet-Prod' or subscription == 'RS-Violet-Non-Prod':
+        netrc_filename=netrc_filename+"_india"
+    else:
+        netrc_filename=netrc_filename+"_us"
+
+    if subscription.find("Non-Prod") == -1:
+        netrc_filename = netrc_filename +"_prod"
+    else:
+        netrc_filename = netrc_filename +"_non-prod"
+
+    file = pathlib.Path(HOME+"/helpers/"+netrc_filename)
+    if file.exists():
+        for root, dirs, files in os.walk(HOME+'/helpers'):
+            if netrc_filename in files:
+                src = os.path.join(root, netrc_filename)
+                copyfile(src, HOME+'/.netrc')
+    else:
+        print("File not exist")
+        sys.exit(201)
+
+location='eastus'
+subscription='Riversand Violet - Non-Production'
+getAuth(location, subscription)
+
+file = open(HOME+'/.netrc')
+
+for line in file:
+    if line.strip():
+        fields = line.strip().split()
+        if fields[1] == 'token':
+            continue
+
+        elif fields[1].endswith('.azuredatabricks.net'):
+            DOMAIN = fields[1]
+
+        else:
+            TOKEN = fields[1]
+    else:
+        continue
+
+print(DOMAIN)
+print(TOKEN)
