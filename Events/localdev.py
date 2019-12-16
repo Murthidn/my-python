a0f95a44e1f95023160923d49d50824d27e0ffa2
Murthidn <murthi267@gmail.com>
2019-12-16 18:05:59 +0530

    Update

--- Events/localdev.py
+++ Events/localdev.py
@@ -7,7 +7,7 @@ try:
     from datetime import datetime
     from datetime import timedelta
     import utilscommon as uc
-    import loadmodules
+    #import loadmodules
     import requests
     import time
     import traceback
@@ -29,28 +29,28 @@ try:
             return rt
 
     def getTotalExternalEvents(taskId, tenant):
-        externalEventsUrl = ('http://rdp-rest:8085/' + tenant + '/api/eventservice/get')
+        externalEventsUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/' + tenant + '/api/eventservice/get')
         externalEventsQuery = {"params": {"query": { "filters": {"typesCriterion": ["externalevent"], "attributesCriterion": [{"taskId": {"exact": taskId}}]}}, "options": {"maxRecords": 1}}}
         externalEventsResponse = requesthelper(externalEventsUrl, externalEventsQuery, headers, TIMEOUT)
         return externalEventsResponse.json()['response']['totalRecords']
 
 
     def getTotalManageEvents(taskId, tenant):
-        manageEventsUrl = ('http://rdp-rest:8085/' + tenant + '/api/eventservice/get')
+        manageEventsUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/' + tenant + '/api/eventservice/get')
         manageEventsQuery = {"params": {"query": {"filters": {"typesCriterion": ["entitymanageevent"], "attributesCriterion": [{"taskId": {"exact": taskId}}]}}, "options": {"maxRecords": 1}}}
         manageEventsResponse = requesthelper(manageEventsUrl, manageEventsQuery, headers, TIMEOUT)
         return manageEventsResponse.json()['response']['totalRecords']
 
 
     def getTotalGovernEvents(taskId, tenant):
-        governEventsUrl = ('http://rdp-rest:8085/' + tenant + '/api/eventservice/get')
+        governEventsUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/' + tenant + '/api/eventservice/get')
         governEventsQuery = {"params": {"query": {"filters": {"typesCriterion": ["entitygovernevent"], "attributesCriterion": [{"taskId": {"exact": taskId}}]}}, "options": {"maxRecords": 1}}}
         governEventsResponse = requesthelper(governEventsUrl, governEventsQuery, headers, TIMEOUT)
         return governEventsResponse.json()['response']['totalRecords']
 
 
     def getTotalRequestObjects(taskId, tenant):
-        requestObjectUrl = ('http://rdp-rest:8085/' + tenant + '/api/requesttrackingservice/get')
+        requestObjectUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/' + tenant + '/api/requesttrackingservice/get')
         requestObjectQuery = {"params": {"query": { "filters": {"typesCriterion": ["requestobject"], "attributesCriterion": [{"taskId": {"exact": taskId}}]}}, "options": {"maxRecords": 1}}}
         requestObjectResponse = requesthelper(requestObjectUrl, requestObjectQuery, headers, TIMEOUT)
         return requestObjectResponse.json()['response']['totalRecords']
@@ -60,13 +60,13 @@ try:
     timeinterval = 60  # min
     tenants = []
     json_body = []
-    env = os.environ['envname']
+    env = 'test' #os.environ['envname']
     measurement = 'integration_report'
     utc_time_from = (datetime.utcnow() - timedelta(minutes=int(timeinterval))).strftime("%Y-%m-%dT%H:%M:00.000-0000")
 
     # Get ALl Tenants
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain', 'x-rdp-userId': 'system_user', 'x-rdp-userRoles': '["admin"]'}
-    tenantGetUrl = ('http://rdp-rest:8085/dataplatform/api/configurationservice/get')
+    tenantGetUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/dataplatform/api/configurationservice/get')
     tenantGetQuery = {'params': {'query': {'filters': {'typesCriterion': ['tenantserviceconfig']}}}}
     tenantGetResponse = requesthelper(tenantGetUrl, tenantGetQuery, headers, TIMEOUT)
 
@@ -76,7 +76,7 @@ try:
     taskSummaryQuery = { "params": { "query": { "filters": { "typesCriterion": [ "tasksummaryobject" ], "propertiesCriterion": [ { "createdDate": { "gt": str(utc_time_from), "type": "_DATETIME" } } ], "attributesCriterion": [ { "taskType": { "exacts": [ "ENTITY_IMPORT", "ENTITY_EXPORT" ] } }, { "status": { "exact": "Processing", "not": "true" } } ] } }, "fields": { "attributes": [ "_ALL" ] } } }
 
     for t in tenants:
-        taskSummaryUrl = ('http://rdp-rest:8085/' + str(t) + '/api/requesttrackingservice/get')
+        taskSummaryUrl = ('http://manage.engg-az-dev2.riversand-dataplatform.com:8085/' + str(t) + '/api/requesttrackingservice/get')
         taskSummaryAPIResponse = requesthelper(taskSummaryUrl, taskSummaryQuery, headers, TIMEOUT)
         if not taskSummaryAPIResponse.json()['response']['totalRecords'] == 0:
             totalTaskSummary = taskSummaryAPIResponse.json()['response']['totalRecords']
@@ -128,7 +128,7 @@ try:
                         "User": taskSummaryData['userId'],
                         "TaskId": taskSummaryData['taskId'],
                         "Status": taskSummaryData['status'],
-                        "Count": taskSummaryData['totalRecords'],
+                        "Import": taskSummaryData['totalRecords'],
                         "LoadError": taskSummaryData['totalLoadError'],
                         "RDPErrors": taskSummaryData['totalRDPErrors'],
                         "ExtractError": taskSummaryData['totalExtractError'],
@@ -147,9 +147,11 @@ try:
                 }
                 ]
                 json_body.extend(json_tmp)
-                time.sleep(3)
-        time.sleep(5)
-    uc.insert_to_influx(json_body, 'app_metrics')
+                print(json_tmp)
+                print('------')
+                #time.sleep(3)
+        #time.sleep(5)
+    #uc.insert_to_influx(json_body, 'app_metrics')
 
 except Exception as error:
     print(error)
