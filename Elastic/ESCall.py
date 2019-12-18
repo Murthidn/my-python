796e3b46ab41ef25033b68f204b97d231338d65c
Murthidn <murthi267@gmail.com>
2019-12-18 12:04:57 +0530

    Update

--- Elastic/ESCall.py
+++ Elastic/ESCall.py
@@ -1,21 +1,23 @@
 import sys
 import loadmodules
 import requests
+import json
 
-def callAPI(url):
-    timeout=10
+
+def requesthelper(url, query, headers, timeout):
     try:
-        r = requests.get(url, timeout=timeout)
-        r.raise_for_status()
-    except requests.exceptions.Timeout as e:
-        return False
+        tenant = requests.post(url, data=json.dumps(query), headers=headers, timeout=timeout)
+    except requests.exceptions.Timeout:
+        print('<br>request timed out (', timeout, 'sec)')
+        sys.exit(201)
     except requests.exceptions.RequestException as e:
-        return False
+        print('<br>exception encountered:', str(e))
+        sys.exit(201)
     else:
-        if r.status_code == 200:
-            return r
+        return tenant
 
-url = 'http://elasticsearch1:9200/_cat/nodes?v'
-
-a = callAPI(url)
-print(a.request)
+url = 'http://elasticsearch1:9200/rdwengg-az-dev2entityindex_v8/_search'
+headers = {'Content-type': 'application/json'}
+query = { "query": { "terms": { "_id": ["fd9ThcZhTUGbCR6XP42FGQ"] } } }
+data = requesthelper(url, query, headers, 60)
+print(data.json())
