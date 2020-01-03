fdd66f957cca8a7ece0b109c342ca6d3c5f8159b
Murthi <murthi267@gmail.com>
2020-01-03 16:42:32 +0530

    Update

---
+++ Switch.py
@@ -0,0 +1,13 @@
+def week(i):
+    switcher = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday'
+    }
+    print (switcher.get(i, "Invalid day of week"))
+
+week(2)
